b244bb1741b540a7118b834d189d395e64165a81
Thales Reis <thales.areis@gmail.com>
2020-05-18 19:37:57 -0300

    fix typo error on index.js

--- src/views/index.js
+++ src/views/index.js
@@ -20,4 +20,3 @@ export { default as CommentsListCoordinator } from './Coordinator/CommentsList';
 export { default as ProfileCoordinator } from './Coordinator/Profile';
 export { default as ReportInteractionHistory} from './ReportInteractionHistory';
 export { default as AproveCityHallList } from './CityHall/Aprove';
-ß
